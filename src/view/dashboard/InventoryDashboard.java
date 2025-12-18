78e8940b17f0456f18ea28927d7c2159105ba80c
Loaay47 <loaaywaheed2020@gmail.com>
2025-12-18 02:13:30 +0200

    add inventory buttons

--- src/view/dashboard/InventoryDashboard.java
+++ src/view/dashboard/InventoryDashboard.java
@@ -74,8 +74,106 @@ public class InventoryDashboard extends Form {
         menu.setBounds(0, 80, 250, getHeight() - 100);
         add(menu);
 
+        JButton addProductBtn = new JButton("Add Product");
+        addProductBtn.setBounds(20, 10, 200, 40);
+        addProductBtn.setBackground(CommonConstants.INVENTORY_COLOR);
+        addProductBtn.setForeground(Color.WHITE);
+        addProductBtn.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+        addProductBtn.setFont(new Font("Dialog", Font.BOLD, 18));
+        addProductBtn.setFocusable(false);
+        addProductBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+            }
+        });
+        menu.add(addProductBtn);
+
+        JButton deleteProductBtn = new JButton("Delete Product");
+        deleteProductBtn.setBounds(20, 70, 200, 40);
+        deleteProductBtn.setBackground(CommonConstants.INVENTORY_COLOR);
+        deleteProductBtn.setForeground(Color.WHITE);
+        deleteProductBtn.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+        deleteProductBtn.setFont(new Font("Dialog", Font.BOLD, 18));
+        deleteProductBtn.setFocusable(false);
+        deleteProductBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+            }
+        });
+        menu.add(deleteProductBtn);
+
+        JButton updateProductBtn = new JButton("Update Product");
+        updateProductBtn.setBounds(20, 130, 200, 40);
+        updateProductBtn.setBackground(CommonConstants.INVENTORY_COLOR);
+        updateProductBtn.setForeground(Color.WHITE);
+        updateProductBtn.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+        updateProductBtn.setFont(new Font("Dialog", Font.BOLD, 18));
+        updateProductBtn.setFocusable(false);
+        updateProductBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+            }
+        });
+        menu.add(updateProductBtn);
+
+        JButton listProductBtn = new JButton("List Products");
+        listProductBtn.setBounds(20, 190, 200, 40);
+        listProductBtn.setBackground(CommonConstants.INVENTORY_COLOR);
+        listProductBtn.setForeground(Color.WHITE);
+        listProductBtn.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+        listProductBtn.setFont(new Font("Dialog", Font.BOLD, 18));
+        listProductBtn.setFocusable(false);
+        listProductBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+            }
+        });
+        menu.add(listProductBtn);
+
+        JButton searchProductBtn = new JButton("Search Products");
+        searchProductBtn.setBounds(20, 250, 200, 40);
+        searchProductBtn.setBackground(CommonConstants.INVENTORY_COLOR);
+        searchProductBtn.setForeground(Color.WHITE);
+        searchProductBtn.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+        searchProductBtn.setFont(new Font("Dialog", Font.BOLD, 18));
+        searchProductBtn.setFocusable(false);
+        searchProductBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+            }
+        });
+        menu.add(searchProductBtn);
+
+        JButton setMinStockBtn = new JButton("Set Min Stock");
+        setMinStockBtn.setBounds(20, 310, 200, 40);
+        setMinStockBtn.setBackground(CommonConstants.INVENTORY_COLOR);
+        setMinStockBtn.setForeground(Color.WHITE);
+        setMinStockBtn.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+        setMinStockBtn.setFont(new Font("Dialog", Font.BOLD, 18));
+        setMinStockBtn.setFocusable(false);
+        setMinStockBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+            }
+        });
+        menu.add(setMinStockBtn);
+
+        JButton manageDamageBtn = new JButton("Manage Damage");
+        manageDamageBtn.setBounds(20, 370, 200, 40);
+        manageDamageBtn.setBackground(CommonConstants.INVENTORY_COLOR);
+        manageDamageBtn.setForeground(Color.WHITE);
+        manageDamageBtn.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+        manageDamageBtn.setFont(new Font("Dialog", Font.BOLD, 18));
+        manageDamageBtn.setFocusable(false);
+        manageDamageBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+            }
+        });
+        menu.add(manageDamageBtn);
+
         JButton productReportBtn = new JButton("Product Report");
-        productReportBtn.setBounds(20, 40, 200, 40);
+        productReportBtn.setBounds(20, 430, 200, 40);
         productReportBtn.setBackground(CommonConstants.INVENTORY_COLOR);
         productReportBtn.setForeground(Color.WHITE);
         productReportBtn.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
@@ -86,43 +184,8 @@ public class InventoryDashboard extends Form {
             public void actionPerformed(ActionEvent e) {
             }
         });
-
         menu.add(productReportBtn);
 
-        JButton specialOfferBtn = new JButton("Special Offer");
-        specialOfferBtn.setBounds(20, 100, 200, 40);
-        specialOfferBtn.setBackground(CommonConstants.INVENTORY_COLOR);
-        specialOfferBtn.setForeground(Color.WHITE);
-        specialOfferBtn.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
-        specialOfferBtn.setFont(new Font("Dialog", Font.BOLD, 18));
-        specialOfferBtn.setFocusable(false);
-        specialOfferBtn.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-            }
-        });
-
-        menu.add(specialOfferBtn);
-
-        JButton sendOfferBtn = new JButton("Offer to INV");
-        sendOfferBtn.setBounds(20, 160, 200, 40);
-        sendOfferBtn.setBackground(CommonConstants.INVENTORY_COLOR);
-        sendOfferBtn.setForeground(Color.WHITE);
-        sendOfferBtn.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
-        sendOfferBtn.setFont(new Font("Dialog", Font.BOLD, 18));
-        sendOfferBtn.setFocusable(false);
-        sendOfferBtn.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-
-                content.removeAll();
-
-                content.revalidate();
-                content.repaint();
-            }
-        });
-
-        menu.add(sendOfferBtn);
         // content area
         content = new JPanel();
         content.setBackground(CommonConstants.BACKGROUND_COLOR);
@@ -131,6 +194,5 @@ public class InventoryDashboard extends Form {
         add(content);
 
     }
-
 
 }
